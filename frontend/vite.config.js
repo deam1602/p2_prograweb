c47734efa352022d2638d0d972624791d19ce068
deam1602 <diegoazurdia16@gmail.com>
2026-05-24 02:04:21 -0600

    infra: conectar pipeline y actualizar cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://skyship-backend-env.eba-rfu7gfxy.us-east-2.elasticbeanstalk.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
